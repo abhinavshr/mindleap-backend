2b22be12f3ea95ce563f1d81d0b4ab9c59ccbefb
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-05-09 08:51:29 +0545

    feat: add DailyMission model for tracking user daily missions and rewards

---
+++ models/DailyMission.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require('sequelize');
+const sequelize     = require('../config/db');
+const User          = require('./User');
+
+const DailyMission = sequelize.define('DailyMission', {
+    id: {
+        type:          DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey:    true,
+    },
+    user_id: {
+        type:       DataTypes.INTEGER,
+        allowNull:  false,
+        references: { model: User, key: 'id' },
+    },
+    mission_key: {
+        type:      DataTypes.STRING(50),
+        allowNull: false,
+    },
+    mission_name: {
+        type:      DataTypes.STRING(100),
+        allowNull: false,
+    },
+    description: {
+        type:      DataTypes.STRING(255),
+        allowNull: false,
+    },
+    xp_reward: {
+        type:      DataTypes.INTEGER,
+        allowNull: false,
+    },
+    completed: {
+        type:         DataTypes.BOOLEAN,
+        allowNull:    false,
+        defaultValue: false,
+    },
+    completed_at: {
+        type:         DataTypes.DATE,
+        allowNull:    true,
+        defaultValue: null,
+    },
+    assigned_date: {
+        type:      DataTypes.DATEONLY,
+        allowNull: false,
+    },
+}, {
+    tableName:  'daily_missions',
+    timestamps: false,
+});
+
+DailyMission.belongsTo(User, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+User.hasMany(DailyMission,   { foreignKey: 'user_id' });
+
+module.exports = DailyMission;
